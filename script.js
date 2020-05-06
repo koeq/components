02ae578e890ded52680357d4911fbc22243a2cd6
koebe1 <koe.be.pay@web.de>
2020-05-06 09:13:08 +0200

    updated form

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// error messages for form //
 const name = document.getElementById('name');
 const password = document.getElementById('password');
 const form = document.getElementById('form');
@@ -5,14 +6,14 @@ const errorElement = document.getElementById('error');
 
 form.addEventListener('submit', (e)=> {
     let messages = [];
-    if (name.value === "" || name.value === null || name.value === "Your Name") {
-        messages.push("Name is required!");
+
+    if (password.value.length < 6) {
+        messages.push("Your password must be at leas 6 characters!");
     }
-    if (password.value === "" || password.value === null || password.value === "Your Password") {
-        messages.push("Password is required!");
-    }
+
     if (messages.length > 0) {
         e.preventDefault();
         errorElement.innerText = messages.join (', ');
+        errorElement.style.border = "1px solid rgba(255, 0, 0, 0.5)";
     }
 })
